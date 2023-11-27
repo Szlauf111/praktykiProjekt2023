75d3f7f83aa25ada2ea382fd823e38fb23555145
plbplb123 <90069475+plbplb123@users.noreply.github.com>
2023-11-27 21:53:02 +0100

    Update Strona_Glowna_Skrypt.js
    Dodanie:
    - Sortowanie przez rok
    - Wyświetlanie gatunków filmu

--- Filmowisko/Skrypty/Strona_Glowna_Skrypt.js
+++ Filmowisko/Skrypty/Strona_Glowna_Skrypt.js
@@ -4,7 +4,6 @@ const API_KEY = 'api_key=c06243e58c51fb3bd88147750a95e9d8'; //Api klucz z tmdb
 const base_url = 'https://api.themoviedb.org/3';
 const discover_url = base_url+'/discover/movie?sort_by=popularity.desc&'+API_KEY; //Link do database'u z filmami
 const searchURL = base_url+'/search/movie?'+API_KEY;
-
 const genres = [
     {
       "id": 28,
@@ -85,22 +84,37 @@ const genres = [
   ]
 
 const main = document.getElementById('main'); // Tam gdzie wszystkie divy z filmami zostaną wysłane.
-const tagsEl = document.getElementById('tags'); 
+const tagsEl = document.getElementById('tags');
+let Sort = 0; 
 
-getmovies(discover_url);
+getmovies(discover_url,Sort);
 
-function getmovies(url)
+function getmovies(url,Sort)
 {
 fetch(url).then(res => res.json()).then(data => { //Bierze filmy z database'u w postaciu arraya
-    showmovies(data.results,"stronaglowna"); //odrazu laduje arraya z wynikiem do funkcji
+          if(Sort == 1)
+          {
+            const sortedarr = data.results.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+            showmovies(sortedarr);
+          }
+          else if(Sort == 2)
+          {
+            const sortedarr = data.results.sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
+            showmovies(sortedarr);
+          }
+          else
+          {
+          showmovies(data.results); 
+          }
+    
 })
 }
 
-function showmovies(data,source)
+function showmovies(data)
 {
     main.innerHTML = '';
-data.forEach(movie =>{ //(przed tem niewiedzialem co to, ale) za kazde miejsce w arrayu, powtarza sie ten kod, odrazu jest strzalkowa z movie:
-    const {title, poster_path, vote_average, overview, id,release_date,vote_count} = movie;
+    data.forEach(movie =>{
+    const {title, poster_path, vote_average, overview, id,release_date,vote_count,genre_ids} = movie;
     //console.log(data);
     const movieEl = document.createElement('div'); //Tworzenie elementow w HTMLu
     movieEl.classList.add('movie'); //dodaje cssa do nowego elementu.
@@ -108,19 +122,24 @@ data.forEach(movie =>{ //(przed tem niewiedzialem co to, ale) za kazde miejsce w
     <img src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}">
     <div class="movieinfo">
         <${GetHeader(title)}>${title}   </${GetHeader(title)}>
+        
         <span class="${getColor(vote_average)}">${Math.round(vote_average* 10)/10}</span>
        
     </div>
-    <h6>Release Date: ${release_date}</h6>
+
+    <h6>Release Date: ${release_date}
+    </h6>
  
     <div class="overview">
     <h4>${title}   </h4>
    Rating: ${Math.round(vote_average* 10)/10} | Vote Count: ${vote_count}<br>
     <br>
     ${shortenText(overview)}<br>
-    <a href="https://www.themoviedb.org/movie/${id}">Zobacz na TMDB</a>
+    <a href="https://www.themoviedb.org/movie/${id}">Zobacz na TMDB</a><br>
+    <h6>${getGenre(genre_ids)}</h6>
+    
     </div>
-
+    
     `
 
     main.appendChild(movieEl); //Wysyla element MovieEl do elementu main
@@ -128,7 +147,17 @@ data.forEach(movie =>{ //(przed tem niewiedzialem co to, ale) za kazde miejsce w
 }
 )
 };
-
+function getGenre(arrgenr)
+{
+  let arr = "";
+  arrgenr.forEach((id) => {
+    const genrefinder = genres.find((genre) => genre.id === id);
+    if (genrefinder) {
+      arr += genrefinder.name + " ";
+    }
+  });
+  return arr;
+}
 function getColor(vote) //self explanatory
 {
     if(vote >= 7)
@@ -151,9 +180,9 @@ form.addEventListener('submit', (e) => { //dodaje mozliwosc wyszukiwania filmow 
     const searchTerm = search.value;
 
     if(searchTerm){
-        getmovies(searchURL+'&query='+searchTerm) //pobiera z inputa slowo klucz i wysyla to do api
+        getmovies(searchURL+'&query='+searchTerm,Sort) //pobiera z inputa slowo klucz i wysyla to do api
     }else{
-        getmovies(discover_url) //wraca do glownej strony z najpopularniejszymi filmami
+        getmovies(discover_url,Sort) //wraca do glownej strony z najpopularniejszymi filmami
     }
 });
 
@@ -193,4 +222,23 @@ function setGenre(){
         })
         tagsEl.append(t);
     })
+}
+function TurnOnSortyear()
+{
+Sort++;
+if(Sort == 1)
+{
+  document.getElementById("SortButton").classList.remove('btn','btn-info');
+  document.getElementById("SortButton").classList.add('btn','btn-success');
+}
+if(Sort == 2)
+{
+  document.getElementById("SortButton").classList.add('btn','btn-danger');
+  document.getElementById("SortButton").classList.remove('btn','btn-success');
+}
+if(Sort == 3){
+  document.getElementById("SortButton").classList.add('btn','btn-info');
+  document.getElementById("SortButton").classList.remove('btn','btn-danger');
+  Sort = 0;
+}
 }
